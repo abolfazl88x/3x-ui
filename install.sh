a23d6421173ffc17df392e7cfb0f158b5041dd7b
abolfazl88x <noisegir0@gmail.com>
2025-07-19 03:04:23 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+read -p "Enter Version (2.5.3)" xversion
+echo "Version entered.: $xversion"
+
+ARCH=$(uname -m)
+case "${ARCH}" in
+  x86_64 | x64 | amd64) XUI_ARCH="amd64" ;;
+  i*86 | x86) XUI_ARCH="386" ;;
+  armv8* | armv8 | arm64 | aarch64) XUI_ARCH="arm64" ;;
+  armv7* | armv7) XUI_ARCH="armv7" ;;
+  *) XUI_ARCH="amd64" ;;
+esac
+
+cd /root/
+wget -O x-ui.tar.gz https://github.com/MHSanaei/3x-ui/releases/download/v${xversion}/x-ui-linux-${XUI_ARCH}.tar.gz
+rm -rf x-ui/ /usr/local/x-ui/ /usr/bin/x-ui
+tar zxvf x-ui.tar.gz
+chmod +x x-ui/x-ui x-ui/bin/xray-linux-* x-ui/x-ui.sh
+cp x-ui/x-ui.sh /usr/bin/x-ui
+cp -f x-ui/x-ui.service /etc/systemd/system/
+mv x-ui/ /usr/local/
+systemctl daemon-reload
+systemctl enable x-ui
+systemctl restart x-ui
+
+echo "✅ X-UI Installed successfully."
